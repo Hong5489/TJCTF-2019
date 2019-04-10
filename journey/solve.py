e17e112ce304587c997a9f1e6c190803a5dbf82b
Hong5489 <46080883+Hong5489@users.noreply.github.com>
2019-04-10 19:50:16 +0800

    Update solve.py

--- journey/solve.py
+++ journey/solve.py
@@ -1,12 +1,15 @@
 from pwn import *
 
 s = remote("p1.tjctf.org",8009)
-f = open("wordList.txt",'w+')
-s.sendline("empress")
+s.sendline("one")
+lastText = ''
 while(1):
-	s.recvuntil("'")
-	text = s.recvuntil("'")[:-1]
-	f.write(text+'\n')
-	s.recvuntil(":")
-	s.sendline(text)
-	print text
+	try:
+		s.recvuntil("'")
+		lastText = s.recvuntil("'")[:-1]
+		s.recvuntil(":")
+		s.sendline(lastText)
+		print lastText
+	except:
+		s.close()
+		s = remote("p1.tjctf.org",8009)
