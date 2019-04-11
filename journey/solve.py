25e29d92200548622e65635a9c1a8ed2a465747c
Hong5489 <hongwei5489@gmail.com>
2019-04-11 22:56:09 +0800

    five

--- journey/solve.py
+++ journey/solve.py
@@ -1,11 +1,8 @@
 from pwn import *
 
 s = remote("p1.tjctf.org",8009)
-<<<<<<< HEAD
-s.sendline("drama")
-=======
+
 s.sendline("one")
->>>>>>> 11abc4a8a9282cb83bb75dc308f663431086e76a
 lastText = ''
 while(1):
 	try:
@@ -14,14 +11,8 @@ while(1):
 		s.recvuntil(":")
 		s.sendline(lastText)
 		print lastText
-<<<<<<< HEAD
 		if lastText.startswith("tjctf"):
 			break
 	except:
 		s.close()
 		s = remote("p1.tjctf.org",8009)
-=======
-	except:
-		s.close()
-		s = remote("p1.tjctf.org",8009)
->>>>>>> 11abc4a8a9282cb83bb75dc308f663431086e76a
